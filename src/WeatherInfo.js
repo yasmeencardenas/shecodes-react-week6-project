a0519cdb782cd367e1870ef0f55760293bfeb2fb
yasmeencardenas <123997412+yasmeencardenas@users.noreply.github.com>
2024-09-29 20:48:46 +0300

    Updated WeatherInfo and created WeatherTemperature

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,4 +1,7 @@
 import React from "react";
+import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
